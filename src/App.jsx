3f47cf637b1b97fd82b3bbe4bdd3ca4092a5c5e3
Oleg Troian <115949684+sleiterr@users.noreply.github.com>
2025-04-30 21:07:25 +0200

    add react-router-dom and set up basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+//?Routes => Renders a branch of <Routes> that best matches the current location.
+//?Route => Renders a branch of Route that best matches the current location.
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import RecipeDetail from "./components/RecipeDetail"; //? Import components
+import Recipes from "./components/Recipes"; //? Import components
+
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    //! <BrowserRouter> is a component for setting up routing in React.
+    <BrowserRouter>
+      <div>
+        <nav>
+          <Link to="/">Home</Link>
+        </nav>
+
+        <div>
+          <Routes>
+            //? path parses a string URL path into its separate pathname,search,andhash components.
+            <Route path="/" element={<Recipes />} />
+            <Route path="/recipe:id" element={<RecipeDetail />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/Recipes.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+//! useParams: Returns an object of key/value pairs of the dynamic params from the current URL
+import { useParams } from "react-router-dom";
+
+const Recipes = () => {
+  return <div>Recipes</div>;
+};
+
+export default Recipes;
