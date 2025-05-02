8266fbee640e92f5f5ee8c27096d9487c4fb5d9d
Oleg <sleite4r@gmail.com>
2025-05-02 11:25:04 +0200

    Filter by mealType, use includes method, add filter button.

--- src/components/RecipeDetail/RecipeDetail.jsx
+++ src/components/RecipeDetail/RecipeDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 //! useParams: Returns an object of key/value pairs of the dynamic params from the current URL
 import { useParams } from "react-router-dom";
+import s from "./RecipeDetail.module.css";
 
 const RecipeDetail = () => {
   const { id } = useParams();
@@ -22,22 +23,28 @@ const RecipeDetail = () => {
 
     fetchRecipe();
   }, [id]);
-  if (!recipe) return <p>Reading...</p>;
+  if (!recipe) return <p className={s.fetch_msg}>Reading...</p>;
 
   return (
-    <div>
-      <h2>{recipe.name}</h2>
-      <img src={recipe.image} alt={recipe.name} />
-      <h4>Ingredients</h4>
-      <ul>
-        {/* i - index of item */}
-        {recipe.ingredients.map((item, i) => (
-          <li key={i}>{item}</li>
-        ))}
-      </ul>
-      <h4>Instructions</h4>
-      <p>{recipe.instructions}</p>
-    </div>
+    <section>
+      <div className={s.about_container}>
+        <div className={s.about_box}>
+          <h2 className={s.about_title}>{recipe.name}</h2>
+          <img className={s.img_about} src={recipe.image} alt={recipe.name} />
+          <h4 className={s.recipe_title}>Ingredients</h4>
+          <ul className={s.about_list}>
+            {/* i - index of item */}
+            {recipe.ingredients.map((item, i) => (
+              <li className={s.item_list} key={i}>
+                {item}
+              </li>
+            ))}
+          </ul>
+          <h4 className={s.about_subtitle}>Instructions</h4>
+          <p className={s.desc_recipe}>{recipe.instructions}</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { handleLike, handleDislike } from "../HandleLike/HandleLikeBtn";
+
 import s from "./Recipies.module.css";
 
 const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
+  const [meailType, setMealType] = useState("all");
+  const [filteredRecipes, setFilteredRecipes] = useState([]);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -13,6 +16,7 @@ const Recipes = () => {
         const res = await fetch("https://dummyjson.com/recipes");
         const data = await res.json();
         setRecipes(data.recipes);
+        setFilteredRecipes(data.recipes);
       } catch (error) {
         console.log("Error fetching recipes:", error);
       }
@@ -20,28 +24,71 @@ const Recipes = () => {
     fetchRecipe();
   }, []);
 
+  const mealTypeFilter = () => {
+    if (meailType === "all") {
+      return recipes;
+    }
+    return recipes.filter((recipe) => recipe.mealType.includes(meailType));
+  };
+
+  const handleMeal = (e) => {
+    const typeMeal = e.target.value;
+    setMealType(typeMeal);
+
+    const filtered = mealTypeFilter(typeMeal);
+    setFilteredRecipes(filtered);
+  };
+
   return (
     <section>
       <div className={s.recipe_container}>
+        <div className={s.filter_btn}>
+          <button className={s.btn_filter} value="all" onClick={handleMeal}>
+            All
+          </button>
+          <button className={s.btn_filter} value="Breakfast" onClick={handleMeal}>
+            Breakfast
+          </button>
+          <button className={s.btn_filter} value="Lunch" onClick={handleMeal}>
+            Lunch
+          </button>
+          <button className={s.btn_filter} value="Dinner" onClick={handleMeal}>
+            Dinner
+          </button>
+        </div>
         <div className={s.list_contsiner}>
-          {recipes.map((recipe) => (
-            <ul className={s.list_recipe} key={recipe.id}>
-              <li>
-                <img src={recipe.image} alt={recipe.name} />
-                <p className={s.list_title}>{recipe.name}</p>
-                <div className={s.recipe_btn }>
-                  <button
-                    className={s.btn_list}
-                    onClick={() => navigate(`/recipe/${recipe.id}`)}
-                  >
-                    Read more
-                  </button>
-                  <button className={ s.btn_like} onClick={() => handleLike(recipe.id)}>🩷</button>
-                  <button className={ s.btn_dislike} onClick={() => handleDislike(recipe.id)}>👎🏻</button>
-                </div>
-              </li>
-            </ul>
-          ))}
+          {filteredRecipes.length > 0 ? (
+            filteredRecipes.map((recipe) => (
+              <ul className={s.list_recipe} key={recipe.id}>
+                <li>
+                  <img src={recipe.image} alt={recipe.name} />
+                  <p className={s.list_title}>{recipe.name}</p>
+                  <div className={s.recipe_btn}>
+                    <button
+                      className={s.btn_list}
+                      onClick={() => navigate(`/recipe/${recipe.id}`)}
+                    >
+                      Read more
+                    </button>
+                    <button
+                      className={s.btn_like}
+                      onClick={() => handleLike(recipe.id)}
+                    >
+                      🩷
+                    </button>
+                    <button
+                      className={s.btn_dislike}
+                      onClick={() => handleDislike(recipe.id)}
+                    >
+                      👎🏻
+                    </button>
+                  </div>
+                </li>
+              </ul>
+            ))
+          ) : (
+            <p>No recipes found</p>
+          )}
         </div>
       </div>
     </section>
